a219d23e8d2e168565bc141918dd8d28879ddf86
Xingrui Song <songx@wustl.edu>
2023-02-01 05:54:15 -0600

    Update config files

--- remote/config_syncthing.py
+++ remote/config_syncthing.py
@@ -5,6 +5,7 @@ import tomli_w
 import xml.etree.ElementTree as ET
 import random
 import subprocess
+import copy
 
 
 dir = os.path.dirname(os.path.realpath(__file__))
@@ -12,7 +13,6 @@ config_path = os.path.join(dir, 'configs\config.toml')
 if os.path.isfile(config_path):
     with open(config_path, "rb") as f:
         config = tomli.load(f)
-        print(config)
 else:
     config = {
         'host': {
@@ -52,14 +52,16 @@ process.wait()
 tree = ET.parse(syncthing_config_path)
 root = tree.getroot()
 
-# Delete the default folder
-for child in root:
-    if child.tag == "folder":
-        root.remove(child)
-
 # Get the sycthing id
 host_id = root.find('defaults').find('folder').find('device').attrib['id']
 config['host']['id'] = host_id
+
+# Clean the devices and folders
+for child in root:
+    if (child.tag == "device") and (child.attrib['id'] != host_id):
+        root.remove(child)
+    elif child.tag == "folder":
+        root.remove(child)
 
 # Set the sycthing name
 for device in root.findall('device'):
@@ -74,6 +76,31 @@ for device in root.findall('device'):
 gui_address = root.find('gui').find('address')
 gui_address.text = '127.0.0.1:' + str(config['syncthing']['port'])
 
+# Add devices
+device_dict = {}
+default_device = root.find('defaults').find('device')
+for device in config['gateways']:
+    device_dict[device['name']] = device['id']
+    new_device = copy.deepcopy(default_device)
+    new_device.attrib['id'] = device['id']
+    new_device.attrib['name'] = device['name']
+    root.append(new_device)
+
+# Add folders
+default_folder = root.find('defaults').find('folder')
+default_folder_device = root.find('defaults').find('folder').find('device')
+for folder in config['networks']:
+    new_folder = copy.deepcopy(default_folder)
+    new_folder.attrib['id'] = folder['id']
+    new_folder.attrib['label'] = folder['name']
+    new_folder.attrib['path'] = f"networks\\{folder['name']}"
+    for device_name in folder['gateways']:
+        if device_name != config['host']['name']:
+            new_folder_device = copy.deepcopy(default_folder_device)
+            new_folder_device.attrib['id'] = device_dict[device_name]
+            new_folder.append(new_folder_device)
+    root.append(new_folder)
+
 # Save
 save_config = True
 if save_config:
@@ -83,8 +110,12 @@ if save_config:
         tree.write(f)
 
     networks_path = os.path.join(dir, 'networks')
-    for network in os.listdir(networks_path):
-        network_path = os.path.join(networks_path, network)
-        if os.path.isdir(network_path):
-            network_config_path = os.path.join(network_path, f'config_{host_id}.toml')
-            shutil.copyfile(config_path, network_config_path)
+    for network in config['networks']:
+        network_path = os.path.join(networks_path, network['name'])
+        if not os.path.exists(network_path):
+            os.makedirs(network_path)
+        remote_configs_path = os.path.join(network_path, 'remote-configs')
+        if not os.path.exists(remote_configs_path):
+            os.makedirs(remote_configs_path)
+        remote_config_path = os.path.join(remote_configs_path, f"{config['host']['name']}.toml")
+        shutil.copyfile(config_path, remote_config_path)
